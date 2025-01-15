ec96f46cc5eb81d3c9fbbe9068a96fb211d1aaeb
adji142 <prasetyoajiw@gmail.com>
2025-01-15 15:10:09 +0700

    asd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -163,9 +163,7 @@ License: You must have a valid license purchased only from themeforest(the above
 </body>
 <!--end::Body-->
 <script>
-	$('#btDaftar').click(function () {
-		// alert('sfasdsa');
-	})
+	
 </script>
 
 </html>
